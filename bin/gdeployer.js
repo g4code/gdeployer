a6a78053a87fcbef287beb5383c6cae9b54b8e5e
Drasko Gomboc <drasko.gomboc@gmail.com>
2014-02-07 16:27:28 +0100

    git interface objects; gdeployer git controls

--- bin/gdeployer.js
+++ bin/gdeployer.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var Gdeployer = require("../lib/gdeployer"),
-program       = require("commander"),
+commander     = require("commander"),
 packageData   = require(__dirname + "/../package.json");
 
-program.version(packageData.version)
+commander.version(packageData.version)
    .usage("[options] [dir]")
-   .option('-c, --config <n>', 'config file path, relative to repository path (default gdeployer.json)')
+   .option('-c, --config <n>',     'config file path, relative to repository path (default gdeployer.json)')
    .option('-r, --repository <n>', 'repository name (default origin)')
-   .option('-b, --branch <n>', 'branch name (default master)')
+   .option('-b, --branch <n>',     'branch name (default master)')
+   .option('-l, --latest <n>',     'latest code (branch || tag) (default branch)')
+   .option('-t, --tag <n>',        'tag name')
    .parse(process.argv);
 
 var gdeployer = new Gdeployer();
-gdeployer.request.args                 = program.args;
-gdeployer.request.configPath           = program.config;
-gdeployer.request.remoteRepositoryName = program.repository;
-gdeployer.request.branchName           = program.branch;
+gdeployer.request.args                 = commander.args;
+gdeployer.request.configPath           = commander.config;
+gdeployer.request.remoteRepositoryName = commander.repository;
+gdeployer.request.branchName           = commander.branch;
+gdeployer.request.latest               = commander.latest;
+gdeployer.request.tag                  = commander.tag;
 gdeployer.run();
